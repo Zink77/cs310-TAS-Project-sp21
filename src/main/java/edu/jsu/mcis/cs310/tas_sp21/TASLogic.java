659225c8eebe10a67e7945e62f2757fb7bf30283
pd-stone <pauldavid.stone1@gmail.com>
2021-04-22 12:08:51 -0500

    Started work on calculateTotalMinutes

--- src/main/java/edu/jsu/mcis/cs310/tas_sp21/TASLogic.java
+++ src/main/java/edu/jsu/mcis/cs310/tas_sp21/TASLogic.java
@@ -5,10 +5,47 @@
  */
 package edu.jsu.mcis.cs310.tas_sp21;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author steph
  */
 public class TASLogic {
-    
+
+    public static int calculateTotalMinutes(ArrayList<Punch> dailypunchlist, Shift shift){
+
+        int totalMinutes = 0;
+        int punchCounter = 0;
+        long clockInTime = 0;
+        long clockOutTime = 0;
+
+        for (int i = 0; i < dailypunchlist.size(); i++){
+
+            /* CLOCK IN */
+            if (dailypunchlist.get(i).getPunchtypeid() == 1){
+
+                clockInTime = dailypunchlist.get(i).getAdjustedTime();
+                punchCounter++;
+            }
+
+            /* CLOCK OUT */
+            if (dailypunchlist.get(i).getPunchtypeid() == 0){
+
+                clockOutTime = dailypunchlist.get(i).getAdjustedTime();
+                punchCounter++;
+            }
+
+            if (clockInTime != 0 && clockOutTime != 0){
+
+                totalMinutes = (int) ((clockOutTime - clockInTime)/60);
+            }
+
+            if (punchCounter == 2)
+                totalMinutes -= shift.getLunchduration();
+        }
+
+        return totalMinutes;
+    }
+
 }
